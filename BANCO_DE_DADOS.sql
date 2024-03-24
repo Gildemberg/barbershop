dc467619ba43c19afb02993542582acbd928d295
Gildemberg Vieira Lopes <gildembergvieiralopes@gmail.com>
2024-03-24 18:49:28 -0300

    atualização do BD

--- BANCO_DE_DADOS.sql
+++ BANCO_DE_DADOS.sql
@@ -35,8 +35,18 @@ CREATE TABLE `agendamento` (
   KEY `AGEND_EMP_COD_idx` (`AGEND_EMP_COD`),
   CONSTRAINT `AGEND_EMP_COD` FOREIGN KEY (`AGEND_EMP_COD`) REFERENCES `empresa` (`EMP_COD`),
   CONSTRAINT `AGEND_USR_COD` FOREIGN KEY (`AGEND_USR_COD`) REFERENCES `usuario` (`USR_COD`)
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `agendamento`
+--
+
+LOCK TABLES `agendamento` WRITE;
+/*!40000 ALTER TABLE `agendamento` DISABLE KEYS */;
+INSERT INTO `agendamento` VALUES (26,2,1,'4/3/2024','11:11'),(27,2,1,'4/3/2024','12:22'),(28,2,1,'7/3/2024','12:22');
+/*!40000 ALTER TABLE `agendamento` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `empresa`
@@ -54,11 +64,26 @@ CREATE TABLE `empresa` (
   `EMP_TEL` varchar(150) DEFAULT NULL,
   `EMP_LOGIN` varchar(150) DEFAULT NULL,
   `EMP_SENHA` varchar(150) DEFAULT NULL,
+  `EMP_DESC` varchar(200) DEFAULT NULL,
+  `EMP_REG1` varchar(45) DEFAULT NULL,
+  `EMP_REG2` varchar(45) DEFAULT NULL,
+  `EMP_REG3` varchar(45) DEFAULT NULL,
+  `EMP_REG4` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`EMP_COD`),
   UNIQUE KEY `EMP_CNPJ` (`EMP_CNPJ`),
   UNIQUE KEY `EMP_NOME` (`EMP_NOME`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `empresa`
+--
+
+LOCK TABLES `empresa` WRITE;
+/*!40000 ALTER TABLE `empresa` DISABLE KEYS */;
+INSERT INTO `empresa` VALUES (1,'123','Barbearia Du\' Mário','Rua da Amora - Av. Castelo Branco - 263 - PA/BA','dumario@gmail.com','(75) 9 9999-0000','dumario','dumario','Inaugurada em 2023, A Barbearia Du\' Mário tem realizado diversos cortes em diversos clientes.','Proibido fumar','Proibido menores de 5','Proibido ofensas','Sem fiado');
+/*!40000 ALTER TABLE `empresa` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `usuario`
@@ -77,8 +102,18 @@ CREATE TABLE `usuario` (
   `USR_SENHA` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`USR_COD`),
   UNIQUE KEY `USR_CPF` (`USR_CPF`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` VALUES (2,'123.123.123-12','Gildemberg Vieira Lopes','gildemberg@gmail.com','(75) 98881-3663','berg','berg');
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -89,4 +124,4 @@ CREATE TABLE `usuario` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-03-24 13:26:56
+-- Dump completed on 2024-03-24 18:46:25
